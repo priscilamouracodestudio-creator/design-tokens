30d03b9ffa9df42d25220a7d7e171dfa9ac7505d
priscilamoura-creator <priscilamouracodestudio@gmail.com>
2025-11-25 19:50:42 -0300

    Inicializacao de projeto e automacao

---
+++ build/js/tokens.js
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const VariableCollectionWhite = "#ffffff";
+export const VariableCollectionGrey400 = "#c1c1c4";
+export const VariableCollectionGrey800 = "#3b3b3b";
+export const VariableCollectionGrey900 = "#1e1e1e";
+export const VariableCollectionGrey950 = "#171717";
+export const VariableCollectionRed900 = "#972f2f";
